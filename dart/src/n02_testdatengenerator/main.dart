4dcef78fb01b705ff9de9c11623dc3a9dce18f28
Eliax00789 <el-p@web.de>
2024-10-02 13:32:59 +0200

    Dart: n02_testdatengenerator: Changed to dynamic DataGenerator implementation selection

--- dart/src/n02_testdatengenerator/main.dart
+++ dart/src/n02_testdatengenerator/main.dart
@@ -3,7 +3,34 @@ import 'dart:io';
 import 'data_generator.dart';
 
 void main() async {
-  await (DataGeneratorCSV()..createData(0, 10)).saveData(File('dataset.csv'));
-  await (DataGeneratorJSON()..createData(0, 10)).saveData(File('dataset.json'));
-  await (DataGeneratorXML()..createData(0, 10)).saveData(File('dataset.xml'));
+  DataGenerator dataGenerator;
+
+  while_input:
+  while (true) {
+    stdout.write('Output as csv, json or xml (c, j, x): ');
+    final String? input = stdin.readLineSync();
+    switch (input) {
+      case 'csv':
+      case 'c':
+        dataGenerator = DataGeneratorCSV();
+        break while_input;
+      case 'json':
+      case 'j':
+        dataGenerator = DataGeneratorJSON();
+        break while_input;
+      case 'xml':
+      case 'x':
+        dataGenerator = DataGeneratorXML();
+        break while_input;
+      default:
+        stdout.write('Invalid input\n');
+        break;
+    }
+  }
+
+  stdout.write('Output filename: ');
+  final String input = stdin.readLineSync() ?? '';
+  final File outputFile = File(input);
+
+  await (dataGenerator..createData(0, 10)).saveData(outputFile);
 }
